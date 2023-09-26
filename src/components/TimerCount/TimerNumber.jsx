aab3bfa486494502ac1f457314514b7f7d22a8cc
StanislavBobrivskyi <stanislavbobrivskyi@gmail.com>
2023-09-26 15:23:14 +0200

    Update TimerNumber.jsx

--- src/components/TimerCount/TimerNumber.jsx
+++ src/components/TimerCount/TimerNumber.jsx
@@ -32,36 +32,36 @@ const TimerNumberFront = styled.div`
   align-items: center;
   justify-content: flex-end;
   backface-visibility: hidden;
-`;
-
-const TimerNumberBack = styled.div`
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: flex-end;
   background-color: #3498db;
-  backface-visibility: hidden;
-  transform: rotateX(180deg);
+  padding: 4px;
+  border-radius: 20px;
+  justify-content: center;
 `;
 
 const flipVariants = {
   front: {
     opacity: 1,
-    rotateX: 180,
+    rotateX: 0,
     transition: {
-      duration: 1,
+      duration: 0.4,
     },
   },
   back: {
-    opacity: 1,
-    rotateX: 0,
+    opacity: 0.5,
+    rotateZ: 90,
+
     transition: {
-      duration: 1,
+      duration: 0.4,
     },
+  },
+};
+
+const numberVariants = {
+  initial: {
+    scale: 1,
+  },
+  animate: {
+    scale: [1, 1.2, 1],
   },
 };
 
@@ -73,7 +73,7 @@ function TimerNumber({ number }) {
 
     const flipTimeout = setTimeout(() => {
       setIsFlipped(false);
-    }, 1000);
+    }, 400);
 
     return () => clearTimeout(flipTimeout);
   }, [number]);
@@ -85,8 +85,14 @@ function TimerNumber({ number }) {
         initial="front"
         animate={isFlipped ? 'back' : 'front'}
       >
-        <TimerNumberFront>{number}</TimerNumberFront>
-        <TimerNumberBack>{number}</TimerNumberBack>
+        <motion.div
+          variants={numberVariants}
+          initial="initial"
+          animate="animate"
+          key={number}
+        >
+          <TimerNumberFront>{number}</TimerNumberFront>
+        </motion.div>
       </TimerNumberWrapper>
     </TimerContainer>
   );
